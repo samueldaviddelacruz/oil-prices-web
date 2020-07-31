a4816f0e7d5ac929c2894ffa35686f209f25dccd
Samuel David De La Cruz <samy.David@clevertech.biz>
2020-07-31 13:46:01 -0400

    added highcharts react wrapper library

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [number,setNumber] = useState(0)
+  const increaseNumber = (event) => {
+    setNumber(number + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{number}</p>
+      <button onClick={increaseNumber}> ok </button>
     </div>
   );
 }
